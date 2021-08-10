c91a94b2f9f2dd103e2f828465331ca7e161b798
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-10 15:20:34 +0300

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,9 +1,10 @@
 from instascrape import *
+from json import dumps, loads
 
 get_instagram_info(username):
-  # Get username
-  username = input('Enter username to scrape: ')
-
+  """
+  Get information about specific username from instagram
+  """
   # Instantiate the scraper objects 
   google = Profile(rf'https://www.instagram.com/{username}/')
   # google_post = Post('https://www.instagram.com/p/CG0UU3ylXnv/')
@@ -14,12 +15,24 @@ get_instagram_info(username):
   google_post.scrape()
   google_hashtag.scrape()
 
-  print(google.followers)
+  # print(google.followers)
   # print(google_post['hashtags'])
-  print(google_hashtag.amount_of_posts)
+  # print(google_hashtag.amount_of_posts)
+  
+  returened_info = {}
+  returened_info['username'] = username
+  returened_info['amount_of_posts'] = google_hashtag.amount_of_posts
+  returened_info['followers'] = google.followers
+  
+  return dumps(returened_info)
 
 def main():
+  # Get username
+  username = input('Enter username to scrape: ')
   
+  # Scrape info
+  json_info = get_instagram_info(username)
+  print(json_info)
 
 if __name__ == '__main__':
   main()
