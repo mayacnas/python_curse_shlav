090c5596ece7b4ddac9c1115cc0ce5e506f61653
Yuval315 <73549896+Yuval315@users.noreply.github.com>
2021-08-10 15:15:49 +0300

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,15 +1,25 @@
 from instascrape import *
 
-# Instantiate the scraper objects 
-google = Profile('https://www.instagram.com/google/')
-google_post = Post('https://www.instagram.com/p/CG0UU3ylXnv/')
-google_hashtag = Hashtag('https://www.instagram.com/explore/tags/google/')
+get_instagram_info(username):
+  # Get username
+  username = input('Enter username to scrape: ')
 
-# Scrape their respective data 
-google.scrape()
-google_post.scrape()
-google_hashtag.scrape()
+  # Instantiate the scraper objects 
+  google = Profile(rf'https://www.instagram.com/{username}/')
+  # google_post = Post('https://www.instagram.com/p/CG0UU3ylXnv/')
+  google_hashtag = Hashtag('https://www.instagram.com/explore/tags/{username}/')
 
-print(google.followers)
-print(google_post['hashtags'])
-print(google_hashtag.amount_of_posts)
+  # Scrape their respective data 
+  google.scrape()
+  google_post.scrape()
+  google_hashtag.scrape()
+
+  print(google.followers)
+  # print(google_post['hashtags'])
+  print(google_hashtag.amount_of_posts)
+
+def main():
+  
+
+if __name__ == '__main__':
+  main()
