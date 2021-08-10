c58922806ea0610901c6f7c570f3e8157e9b141d
root <root@localhost.localdomain>
2021-08-10 07:23:30 -0400

    write facebook posts to database

--- script.py
+++ script.py
@@ -1,13 +1,25 @@
 from pymongo import MongoClient
+import json
+import os
+from facebook_scraper import get_posts
 client = MongoClient('mongodb://localhost:27017/')
 mydatabase = client['facebook']
 mycollection = mydatabase['posts']
-post={
-'user': 'user',
-'content': 'hello',
-'date': ,
-'likes': 104,
-'comments': 103,
-'shares': 102
-}
-mydatabase.mycollection.insert(post)
+page=input("hey please enter the page you want to take posts from: ")
+for post in get_posts(page, pages=1):
+    facebook_post={
+    'username': post['username'],
+    'text': post['text'],
+    'date': str(post['time']),
+    'likes': post['likes'],
+    'comments': post['comments'],
+    'shares': post['shares']
+    }
+    json_object = json.dumps(facebook_post, indent = 4)
+    jsonFile = open("data.json", "w")
+    jsonFile.write(json_object)
+    jsonFile.close()
+    with open('data.json') as f:
+        data = json.load(f)
+    x = mycollection.insert_one(data)
+os.remove("data.json")
