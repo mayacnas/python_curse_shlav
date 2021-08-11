47de80282c138b5a5b655886394b7788df8e6082
noyvilkin <88708514+noyvilkin@users.noreply.github.com>
2021-08-11 10:40:31 +0300

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,8 +1,8 @@
 #1 - noy
 import pandas as pd
-read_file = pd.read_excel (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.xlsx", sheet_name='in')
-read_file.to_csv (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv", index = None, header=True)
-df = pd.read_csv(r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv")
+read_file = pd.read_excel ("Automobile_data.xlsx", sheet_name='in')
+read_file.to_csv ("Automobile_data.csv", index = None, header=True)
+df = pd.read_csv("Automobile_data.csv")
 print(df.head(5))
 print(df.tail(5))
 
