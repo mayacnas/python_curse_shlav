71e778382d42c243d193f0aba81d2ee059c3d1eb
blaus23 <88371875+blaus23@users.noreply.github.com>
2021-08-11 10:09:52 +0300

    add exercise 6

--- pandas.py
+++ pandas.py
@@ -5,3 +5,10 @@ read_file.to_csv (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv", i
 df = pd.read_csv(r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv")
 print(df.head(5))
 print(df.tail(5))
+
+#6
+import pandas as pd
+df = pd.read_csv('Automobile_data.csv')
+df2 = df.groupby('company').max('price')
+df2['company'] = df2.index
+print(df2[['company', 'price']])
