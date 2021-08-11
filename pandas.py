918c2cd82df3ec6e00563625d6e876442dac548c
Amit Ben Zur <amitbz1998@gmail.com>
2021-08-11 10:17:44 +0300

    question 7

--- pandas.py
+++ pandas.py
@@ -5,3 +5,8 @@ read_file.to_csv (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv", i
 df = pd.read_csv(r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv")
 print(df.head(5))
 print(df.tail(5))
+
+
+#question 7
+excelData = pd.read_excel('Automobile_data.xlsx', index_col=0)
+print(excelData.groupby(['company'])['average-mileage'].agg(lambda x: x.unique().sum()/x.nunique()))
