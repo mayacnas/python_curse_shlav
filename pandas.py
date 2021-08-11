c13e1ef318e818c8ae50436402715c410a238910
noyvilkin <88708514+noyvilkin@users.noreply.github.com>
2021-08-11 10:16:26 +0300

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,4 +1,4 @@
-#1 
+#1 - noy
 import pandas as pd
 read_file = pd.read_excel (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.xlsx", sheet_name='in')
 read_file.to_csv (r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv", index = None, header=True)
@@ -6,7 +6,12 @@ df = pd.read_csv(r"C:\Users\Noy Vilkin\Documents\python\Automobile_data.csv")
 print(df.head(5))
 print(df.tail(5))
 
-#6
+#3 - noy
+high_val = df.sort_values(by="price", ascending=False)
+high_val = high_val[["company", "price"]]
+print("Highest price:\n", high_val.head(1))
+
+#6 - ofir
 import pandas as pd
 df = pd.read_csv('Automobile_data.csv')
 df2 = df.groupby('company').max('price')
